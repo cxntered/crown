547046c51ab96137d7de0758d28864b743020ffc
cxntered <67499489+cxntered@users.noreply.github.com>
2022-07-05 00:15:02 +0800

    added upload button + added upload/download icons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,118 @@
 const addCrown = async () => {
     try {
         document.getElementById("download").style.visibility = "hidden";
+
         const ign = document.getElementById("ign").value
-        const uuid = await fetch(`https://playerdb.co/api/player/minecraft/${ign}`).then(res => res.json());
-        if (!uuid.success) {
-            return alert("ERROR: An error has occured. Make sure that you inputted the right IGN/UUID.")
+        const skinFile = document.getElementById("skin-file").files[0];
+
+        if (!ign && !skinFile) {
+            return alert("ERROR: You have not inputted an IGN/UUID or a skin file!")
         }
 
-        canvas = document.getElementById("canvas");
-        ctx = canvas.getContext("2d");
-
-        setTimeout(() => {
-            let crown = new Image();
-            crown.crossOrigin = "anonymous";
-            crown.onload = function() {
-                ctx.imageSmoothingEnabled = false;
-                ctx.drawImage(crown, 0, 0);
-
-                const downloadURL = canvas.toDataURL();
-                document.getElementById("downloadLink").href = downloadURL;
-                document.getElementById("download").style.visibility = "visible";
-            };
-            crown.src = "./assets/images/crown.png";
-        }, 1500);
-
-        let skin = new Image();
-        skin.crossOrigin = "anonymous";
-        skin.onload = function() {
-            if (skin.height === 32) {
-                canvas.width = 64;
-                canvas.height = 32;
-            } else {
-                canvas.width = 64;
-                canvas.height = 64;
+        if (ign && !skinFile || ign && skinFile) {
+            if (ign && !skinFile) console.log('ign but no skin file')
+            if (ign && skinfile) console.log('ign and skin file')
+            const uuid = await fetch(`https://playerdb.co/api/player/minecraft/${ign}`).then(res => res.json());
+            if (!uuid.success) {
+                return alert("ERROR: An error has occured. Make sure that you inputted the right IGN/UUID.")
             }
 
-            ctx.imageSmoothingEnabled = false;
-            ctx.drawImage(skin, 0, 0);
-        };
-        skin.src = `https://crafatar.com/skins/${uuid.data.player.id}`;
+            canvas = document.getElementById("canvas");
+            ctx = canvas.getContext("2d");
+            displayCanvas = document.getElementById("displayCanvas");
+            dCtx = displayCanvas.getContext("2d");
+
+            const drawCrown = () => {
+                let crown = new Image();
+                crown.crossOrigin = "anonymous";
+                crown.onload = function() {
+                    ctx.imageSmoothingEnabled = false;
+                    dCtx.imageSmoothingEnabled = false;
+                    ctx.drawImage(crown, 0, 0);
+                    dCtx.drawImage(canvas, 0, 0, 128, skin.height === 32 ? 64 : 128);
+
+                    const downloadURL = canvas.toDataURL();
+                    document.getElementById("downloadLink").href = downloadURL;
+                    document.getElementById("download").style.visibility = "visible";
+                };
+                crown.src = "./assets/images/crown.png";
+            };
+
+            let skin = new Image();
+            skin.crossOrigin = "anonymous";
+            skin.onload = function() {
+                if (skin.height === 32) {
+                    canvas.width = 64;
+                    canvas.height = 32;
+                    displayCanvas.width = 128;
+                    displayCanvas.height = 64;
+                } else {
+                    canvas.width = 64;
+                    canvas.height = 64;
+                    displayCanvas.width = 128;
+                    displayCanvas.height = 128;
+                }
+
+                ctx.imageSmoothingEnabled = false;
+                ctx.drawImage(skin, 0, 0);
+                drawCrown();
+            };
+            skin.src = `https://crafatar.com/skins/${uuid.data.player.id}`;
+        } else if (!ign && skinFile) {
+            console.log(skinFile)
+            console.log('no ign but skin file')
+            canvas = document.getElementById("canvas");
+            ctx = canvas.getContext("2d");
+            displayCanvas = document.getElementById("displayCanvas");
+            dCtx = displayCanvas.getContext("2d");
+
+            const drawCrown = () => {
+                let crown = new Image();
+                crown.crossOrigin = "anonymous";
+                crown.onload = function() {
+                    ctx.imageSmoothingEnabled = false;
+                    dCtx.imageSmoothingEnabled = false;
+                    ctx.drawImage(crown, 0, 0);
+                    dCtx.drawImage(canvas, 0, 0, 128, skin.height === 32 ? 64 : 128);
+
+                    const downloadURL = canvas.toDataURL();
+                    document.getElementById("downloadLink").href = downloadURL;
+                    document.getElementById("download").style.visibility = "visible";
+                };
+                crown.src = "./assets/images/crown.png";
+            };
+
+            let skin = new Image();
+            skin.crossOrigin = "anonymous";
+            skin.onload = function() {
+                if (skin.height === 32) {
+                    canvas.width = 64;
+                    canvas.height = 32;
+                    displayCanvas.width = 128;
+                    displayCanvas.height = 64;
+                } else {
+                    canvas.width = 64;
+                    canvas.height = 64;
+                    displayCanvas.width = 128;
+                    displayCanvas.height = 128;
+                }
+
+                ctx.imageSmoothingEnabled = false;
+                ctx.drawImage(skin, 0, 0);
+                drawCrown();
+            }
+            skin.src = URL.createObjectURL(skinFile);
+        }
     } catch (err) {
         console.log(err);
         alert("ERROR: An error has occured. Check the console for more information. If this persists, contact cxntered @ cxntered#8012 on Discord.")
     }
 }
+
+const skinFile = document.getElementById("skin-file");
+
+const fileChosen = document.getElementById("file-chosen");
+
+skinFile.addEventListener("change", function() {
+    fileChosen.textContent = this.files[0].name
+})
