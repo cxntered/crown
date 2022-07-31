96b67d3bae3bb7ae29d87c4b1bdbb6b1994e501e
cxntered <cxntered@gmail.com>
2022-08-01 06:04:58 +1000

    zoinks!

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,18 +2,18 @@ const warningAlert = (msg) => {
     document.getElementsByClassName("alert")[0].style.backgroundColor = "#ffaa2c";
     document.getElementsByClassName("alert")[0].style.visibility = "visible";
     document.getElementsByClassName("alert")[0].style.opacity = "1";
-    document.getElementsByClassName("alert")[0].innerHTML = `<span class="closebtn" onclick="this.parentElement.style.opacity = '0'; setTimeout(function(){ this.parentElement.style.visibility = 'hidden'; }, 150)"> &times;</span>${msg}`;
+    document.getElementsByClassName("alert")[0].innerHTML = `<span class="closebtn" onclick="this.parentElement.style.opacity = '0'; setTimeout(function(){ this.parentElement.style.visibility = 'hidden'; }, 150)"> &times;</span><b>WARNING:</b> <br />${msg}`;
 };
 
 const errorAlert = (msg) => {
     document.getElementsByClassName("alert")[0].style.backgroundColor = "#f44336";
     document.getElementsByClassName("alert")[0].style.visibility = "visible";
     document.getElementsByClassName("alert")[0].style.opacity = "1";
-    document.getElementsByClassName("alert")[0].innerHTML = `<span class="closebtn" onclick="this.parentElement.style.opacity = '0'; setTimeout(function(){ this.parentElement.style.visibility = 'hidden'; }, 150)"> &times;</span>${msg}`;
+    document.getElementsByClassName("alert")[0].innerHTML = `<span class="closebtn" onclick="this.parentElement.style.opacity = '0'; setTimeout(function(){ this.parentElement.style.visibility = 'hidden'; }, 150)"> &times;</span><b>ERROR:</b> <br />${msg}`;
 };
 
 if (window.matchMedia("(max-width: 480px)").matches) {
-    warningAlert("WARNING: We have detected that you may be on a mobile device or simply have a small screen. You can still use the website, but please know that this website is not fully optimized for mobile devices, meaning there might be more bugs than on a computer or simply bad UI/UX. Please use a desktop or laptop computer to get the full experience.");
+    warningAlert("We have detected that you may be on a mobile device or simply have a small screen. You can still use the website, but please know that this website is not fully optimized for mobile devices, meaning there might be more bugs than on a computer or simply bad UI/UX. Please use a desktop or laptop computer to get the full experience.");
 };
 
 const addCrown = async () => {
@@ -24,13 +24,13 @@ const addCrown = async () => {
         const skinFile = document.getElementById("skin-file").files[0];
 
         if (!ign && !skinFile) {
-            return errorAlert("ERROR: You have not inputted an IGN/UUID or a skin file!")
+            return errorAlert("You have not inputted an IGN/UUID or a skin file!")
         }
 
         if (ign && !skinFile || ign && skinFile) {
             const uuid = await fetch(`https://playerdb.co/api/player/minecraft/${ign}`).then(res => res.json());
             if (!uuid.success) {
-                return errorAlert("ERROR: An error has occured. Make sure that you inputted the right IGN/UUID.")
+                return errorAlert("An error has occured. Make sure that you inputted the right IGN/UUID.")
             }
 
             canvas = document.getElementById("canvas");
@@ -143,7 +143,7 @@ const addCrown = async () => {
         }
     } catch (err) {
         console.log(err);
-        errorAlert("ERROR: An error has occured. Check the console for more information. If this persists, contact cxntered @ cxntered#8012 on Discord & take a screenshot of the console. (screenshot.help to take a screenshot)")
+        errorAlert("An error has occured. <a class=\"alertLink\" href=\"https://balsamiq.com/support/faqs/browserconsole/\">Check the Developer Console for more information</a>. If this persists, take a screenshot of the console and <a class=\"alertLink\" href=\"https://github.com/cxntered/crown/issues\">create an issue on the GitHub repo</a>.")
     };
 };
 
